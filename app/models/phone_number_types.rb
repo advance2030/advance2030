b6472cc439ca064d9650cfd344c84882df7861e6
Joe Fiorini <joe@faithfulgeek.org>
2010-02-27 17:07:03 -0500

    Singularize plural models

--- app/models/phone_number_types.rb
+++
@@ -1,2 +0,0 @@
-class PhoneNumberTypes < ActiveRecord::Base
-end
--- spec/models/email_address_type_spec.rb
+++ spec/models/email_address_type_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe EmailAddressTypes do
+describe EmailAddressType do
   before(:each) do
     @email_address_type = Factory.build(:email_address_type)
   end
