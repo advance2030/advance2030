027fc8fa338aab22b4acac9de05762a84e903041
Joe Fiorini <joe@faithfulgeek.org>
2010-02-27 14:15:01 -0500

    Remove erubis

--- config/initializers/erubis.rb
+++
@@ -1,5 +0,0 @@
-require 'erubis/helpers/rails_helper'
-require 'cleveland_2030_club/eruby'
-
-Erubis::Helpers::RailsHelper.engine_class = Cleveland2030Club::Eruby
-
--- lib/cleveland_2030_club/eruby.rb
+++
@@ -1,6 +0,0 @@
-module Cleveland2030Club
-  class Eruby < Erubis::Eruby
-    include Erubis::EscapeEnhancer
-
-  end
-end
