5e25ce42eb2b38b899aff8190bf6761a5ec77e6a
Nate Klaiber <nate@theklaibers.com>
2010-02-27 19:02:24 -0500

    Adding committee migration

---
+++ db/migrate/20100227223248_create_committees.rb
@@ -0,0 +1,44 @@
+class CreateCommittees < ActiveRecord::Migration
+  def self.up
+    create_table :committees do |t|
+      t.string :name, :null => false, :length => 50
+      t.string :description, :null => false
+      t.boolean :active
+    end
+    
+    create_table :committee_members do |t|
+      t.integer :member_id, :null => false
+      t.integer :committee_id, :null => false
+      t.boolean :lead, :null => false, :default => false
+    end
+    
+    create_table :committee_posts do |t|
+      t.string  :title, :null => false, :length => 100
+      t.integer :user_id, :null => false
+      t.string  :text, :null => false
+    end
+    
+    create_table :committee_assets do |t|
+      t.integer :asset_id, :null => false
+      t.integer :committee_id, :null => false
+    end
+    
+    create_table :committee_events do |t|
+      t.integer :event_id, :null => false
+      t.integer :committee_id, :null => false
+    end
+  
+    create_table :committee_blogs do |t|
+      t.string  :title, :null => false, :length => 100
+      t.integer :user_id, :null => false
+      t.string  :text, :null => false
+  end
+
+  def self.down
+    drop_table :committee_events
+    drop_table :committee_assets
+    drop_table :committee_posts
+    drop_table :committee_members
+    drop_table :committees
+  end
+end
