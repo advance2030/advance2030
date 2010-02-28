d506b9133acd403e6f172d5c3e6ab1d82c9f83d4
Joe Fiorini <joe@faithfulgeek.org>
2010-02-28 14:35:40 -0500

    Render new events view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,12 @@
 class EventsController < ApplicationController
 
   def new
+    @event = Event.new
+    render :action => :new
   end
+
+  def presenter
+    @presenter ||= Presenters::EventForm.new
+  end
+
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -9,7 +9,19 @@ describe EventsController do
     end
 
     subject { response }
+
     it { should be_success }
+    it { should render_template("events/new") }
+
+    it "routes correctly" do
+      "/events/new".should route_to(:controller => "events", :action => "new")
+    end
+
+    it "has a new event" do
+      assigns[:event].should_not be_nil
+      assigns[:event].should be_new_record
+    end
+
   end
 
 end
