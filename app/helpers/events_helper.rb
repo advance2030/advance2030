18ebe11e30295a43b51d8ea827a619b15edee27f
Joe Fiorini <joe@faithfulgeek.org>
2010-02-28 14:55:50 -0500

    Link to new venue page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+
+  def venue_label_text
+    %(Venue <span class="aside-label">(If you do not see your venue you can #{link_to_new_venue("add a new a venue here")})</span>)
+  end
+
+  def link_to_new_venue(text)
+    link_to(text, new_venue_path)
+  end
+
 end
