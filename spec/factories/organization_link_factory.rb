24732dbd56723e535d3d0435fb2fe271201d836f
Nate Klaiber <nate@theklaibers.com>
2010-02-28 12:13:27 -0500

    factory and spec for org links

---
+++ spec/factories/organization_link_factory.rb
@@ -0,0 +1,7 @@
+Factory.define(:organization_link) do |f|
+  f.link_type_id 1
+  f.organization_id 1
+  f.link_id 2
+  f.is_primary true
+  f.sort_order 1
+end
--- spec/models/organization_link_spec.rb
+++ spec/models/organization_link_spec.rb
@@ -2,16 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe OrganizationLink do
   before(:each) do
-    @valid_attributes = {
-      :link_type_id => 1,
-      :organization_id => 1,
-      :link_id => 1,
-      :is_primary => false,
-      :sort_order => 1
-    }
+    @organization_link = Factory.build(:organization_link)
   end
 
   it "should create a new instance given valid attributes" do
-    OrganizationLink.create!(@valid_attributes)
+    @organization_link.should be_valid
   end
 end
