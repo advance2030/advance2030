75e3935a65fda5128c3bd1903bd62122f048e6ab
Kevin Solorio <kevin@detone8.com>
2010-05-29 00:34:47 -0400

    added class current to current link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def current_link(name, options={}, html_options={})
+    if current_page?(options)
+      link_to(name, options, html_options.merge({:class => 'current'}))
+    else
+      link_to(name, options, html_options)
+    end
+  end
+  
+  
 end
