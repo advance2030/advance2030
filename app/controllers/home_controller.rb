09080d7d51c1946cf142f3f9196d0e13d6fdc697
Kevin Solorio <kevin@detone8.com>
2010-05-31 16:50:25 -0400

    sponsored events all set, needs testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,13 @@ class HomeController < ApplicationController
 
   def index
     @coming_events = Event.by_range(Time.now, Time.now.advance(:days => 14))
-    @sponsored_events = @coming_events.sponsored
-    @unsponsored_events = @coming_events - @coming_sponsored_events
+    unless @coming_events.empty?    
+      @sponsored_events = @coming_events.sponsored
+      @unsponsored_events = @coming_events - @coming_sponsored_events
+    else
+      @sponsored_events = []
+      @unsponsored_events = []
+    end
   end
   
 end
