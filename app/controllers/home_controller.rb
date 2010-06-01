4a616966a11267e169a4b5cc4fc46fc1d6f2fb38
Kevin Solorio <kevin@detone8.com>
2010-05-31 22:00:47 -0400

    corrected instance variable name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @coming_events = Event.by_range(Time.now, Time.now.advance(:days => 14))
     unless @coming_events.empty?    
       @sponsored_events = @coming_events.sponsored
-      @unsponsored_events = @coming_events - @coming_sponsored_events
+      @unsponsored_events = @coming_events - @sponsored_events
     else
       @sponsored_events = []
       @unsponsored_events = []
