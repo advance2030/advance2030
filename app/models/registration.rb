bb409c507b277133a97faff788c2a532d84141b6
adomokos <adomokos@gmail.com>
2010-06-02 21:52:24 -0400

    Added validation to the Register model

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -4,5 +4,6 @@ class Registration < ActiveRecord::Base
   attr_accessor :password_confirmation
   
   belongs_to :account
-  
+ 
+  validates_presence_of :login, :first_name, :last_name, :email_address
 end
