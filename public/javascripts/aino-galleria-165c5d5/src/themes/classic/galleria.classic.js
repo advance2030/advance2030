06287d0aa43d176253d1283827a17a21068816ef
matthew orcutt <morcutt@within3.com>
2010-06-05 19:23:04 -0400

    added js library for flickr integration added basic styles for handling errors

---
+++ public/javascripts/aino-galleria-165c5d5/src/themes/classic/galleria.classic.js
@@ -0,0 +1,50 @@
+Galleria.themes.create({
+    name: 'classic',
+    author: 'Galleria',
+    version: '1.1',
+    css: 'galleria.classic.css',
+    defaults: {
+        transition: 'slide'
+    },
+    init: function(options) {
+
+        this.$('loader').show().fadeTo(200, .4);
+        this.$('counter').show().fadeTo(200, .4);
+        
+        this.$('thumbnails').children().hover(function() {
+            $(this).not('.active').fadeTo(200, 1);
+        }, function() {
+            $(this).not('.active').fadeTo(400, .4);
+        }).not('.active').css('opacity',.4);
+        
+        this.$('container').hover(this.proxy(function() {
+            this.$('image-nav-left,image-nav-right,counter').fadeIn(200);
+        }), this.proxy(function() {
+            this.$('image-nav-left,image-nav-right,counter').fadeOut(500);
+        }));
+        
+        this.$('image-nav-left,image-nav-right,counter').hide();
+        
+        var elms = this.$('info-link,info-close,info-text').click(function() {
+            elms.toggle();
+        });
+        
+        this.bind(Galleria.LOADSTART, function(e) {
+            if (!e.cached) {
+                this.$('loader').show().fadeTo(200, .4);
+            }
+            if (this.hasInfo()) {
+                this.$('info').show();
+            } else {
+                this.$('info').hide();
+            }
+            $(e.thumbTarget).parent().addClass('active').css('opacity',1)
+                .siblings('.active').removeClass('active').fadeTo(400,.4);
+        });
+
+        this.bind(Galleria.LOADFINISH, function(e) {
+            this.$('loader').fadeOut(200);
+            $(e.thumbTarget).css('opacity',1)
+        });
+    }
+});
