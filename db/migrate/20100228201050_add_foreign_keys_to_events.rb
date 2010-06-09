ebf6e627d60136bfbb38b054dfc741106c5e6671
adomokos <adomokos@gmail.com>
2010-06-08 23:44:20 -0400

    Trying to fix the breaking db build on Heroku

--- db/migrate/20100228201050_add_foreign_keys_to_events.rb
+++ db/migrate/20100228201050_add_foreign_keys_to_events.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToEvents < ActiveRecord::Migration
   def self.up
-    alter_table :events do |t|
+    change_table :events do |t|
       t.integer :venue_id
       t.integer :manager_id
       t.integer :supervisor_id
