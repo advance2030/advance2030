8dba85a02513f170f88f074e66e98bc5ccf0a1fd
matthew orcutt <morcutt@within3.com>
2010-06-27 17:38:23 -0400

    added subpages to rspec

--- spec/controllers/benefits_controller_spec.rb
+++ spec/controllers/benefits_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe benefitsController do
+describe BenefitsController do
 
   #Delete these examples and add some real ones
-  it "should use benefitsController" do
-    controller.should be_an_instance_of(benefitsController)
+  it "should use BenefitsController" do
+    controller.should be_an_instance_of(BenefitsController)
   end
 
 
@@ -15,9 +15,51 @@ describe benefitsController do
     end
   end
 
-  describe "GET 'directors'" do
+  describe "GET 'social'" do
     it "should be successful" do
-      get 'directors'
+      get 'social'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'networking'" do
+    it "should be successful" do
+      get 'networking'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'philanthropic'" do
+    it "should be successful" do
+      get 'philanthropic'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'educational'" do
+    it "should be successful" do
+      get 'educational'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'involved'" do
+    it "should be successful" do
+      get 'involved'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'discounts'" do
+    it "should be successful" do
+      get 'discounts'
+      response.should be_success
+    end
+  end
+  
+    describe "GET 'join'" do
+    it "should be successful" do
+      get 'join'
       response.should be_success
     end
   end
