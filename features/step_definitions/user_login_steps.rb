9304ff5e36907590e2a07b5f6171ac5f7222586d
Attila Domokos <adomokos@gmail.com>
2010-07-13 18:41:42 -0400

    DRY-ed up the cucumber step

--- features/step_definitions/user_login_steps.rb
+++ features/step_definitions/user_login_steps.rb
@@ -1,27 +1,12 @@
 Given /^the following user records in the data store$/ do |table|
-  registration_converter = ConvertsRegistrationToAccountInformation.new
-  table.hashes.each do |hash|
-    registration = Registration.new(hash)
-    registration.password_confirmation = hash["password"]
-
-    # add address info
-    user_address = Factory(:user_address)
-    registration.address = user_address.address
-    registration.address2 = user_address.address2
-    registration.city = user_address.city
-    registration.state = user_address.state_code
-    registration.zip = user_address.zip
-
-    registration_converter.do_it(registration)
-    # Activate the account for now...
-    account = Account.find_by_login(hash["login"])
-    account.active = true
-    account.save!
-  end
-
+  create_users(table, true)
 end
 
- Given /^the following inactive user records in the data store$/ do |table|
+Given /^the following inactive user records in the data store$/ do |table|
+  create_users(table, false)
+end
+
+def create_users(table, active)
   registration_converter = ConvertsRegistrationToAccountInformation.new
   table.hashes.each do |hash|
     registration = Registration.new(hash)
@@ -38,7 +23,7 @@ end
     registration_converter.do_it(registration)
     # Activate the account for now...
     account = Account.find_by_login(hash["login"])
-    account.active = false
+    account.active = active
     account.save!
   end
 end
